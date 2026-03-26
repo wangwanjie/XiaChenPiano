be51429e795cf65b15ff3967982f180c525d959c
VanJay <vanjay.dev@gmail.com>
2026-03-26 23:00:10 +0800

    Initial Commit

---
+++ XiaChenPiano/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XiaChenPiano
+//
+//  Created by VanJay on 2026/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
